ee22fd0a327656ea3722cd8168cd9acea7840cf5
prads01 <praduyt01@gmail.com>
2020-09-19 09:12:15 +0530

    Initial Commit

---
+++ Chat Box/Chat_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_BoxApp.swift
+//  Chat Box
+//
+//  Created by Praduyt Sen on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Chat_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
